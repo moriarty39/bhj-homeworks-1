e2908f9a1b68fcf3e06143446f1dd91647d6bf24
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-08-12 12:50:20 -0400

    Update task.js

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,6 +68,13 @@ class Autocomplete {
   }
 
   getMatches( text ) {
+    let result = [];
+    const arrOptons = Array.from(this.input.options);
+    arrOptons.forEach(option => {
+      if(option.text.includes(text)) {
+        result.push({text: option.text, value: option.value})
+      }
+    })
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
