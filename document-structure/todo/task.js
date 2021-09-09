5f8d43ca82a6841ea7180359301d771f52eab3cd
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-09-09 13:06:03 -0400

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,33 +1,72 @@
-const taskAdd = document.getElementById('tasks__add');
-const tasksList = document.getElementById('tasks__list');
-const taskInput = document.getElementById('task__input');
+"use strict";
 
-taskAdd.addEventListener('click', (event)=>{
-    event.preventDefault();
-    if(taskInput.value !== ''){
-        tasksList.innerHTML += 
-        `<div class="task">
-            <div class="task__title">
-                ${taskInput.value}
-            </div>
-            <a href="#" class="task__remove">&times;</a>
-        </div>`
-    
-        taskInput.value = '';
-    
-        const task = Array.from(document.querySelectorAll('.task'));
-        const close = Array.from(document.querySelectorAll('.task__remove'));
-    
-        task.forEach((element, index) =>{
-            localStorage.setItem(index, element.innerText);
-        })
-    
-        close.forEach(element => {
-            element.addEventListener('click', (event)=> {
-                event.target.parentElement.remove();
-                localStorage.removeItem(event);
-            })
-        })
+const btnTaskAdd = document.getElementById("tasks__add");
+const taskInput = document.getElementById("task__input");
+const taskList = document.getElementById("tasks__list");
+let taskIdCounter;
+
+if (localStorage.getItem("lastTaskId")) {
+  for (let i = 0; i <= localStorage.getItem("lastTaskId"); i++) {
+    if (localStorage.getItem(`task${i}`)) {      
+      let data = localStorage.getItem(`task${i}`);
+      taskList.insertAdjacentHTML(
+        "beforeEnd",
+        `
+        <div class="task" data-task-id="task${i}">
+        <div class="task__title">
+        ${data}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+       </div>
+       `
+      );
     }
-    
-})
+  }
+  taskIdCounter = +localStorage.getItem("lastTaskId") + 1;
+} else {
+  taskIdCounter = 0;
+}
+
+function addTask() {
+ if (notEmpty(taskInput.value)) {
+  taskList.insertAdjacentHTML(
+    "beforeEnd",
+    `
+   <div class="task" data-task-id="task${taskIdCounter}">
+    <div class="task__title">
+     ${taskInput.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+   </div>
+   `
+  );
+  localStorage.setItem("lastTaskId", taskIdCounter);
+  localStorage.setItem(`task${taskIdCounter++}`, taskInput.value);
+ } 
+}
+
+function notEmpty(data) {
+ return (data.trim() != "");
+}
+
+btnTaskAdd.addEventListener("click", event => {
+ event.preventDefault();
+ addTask();
+ taskInput.value = "";
+});
+
+taskInput.addEventListener("keydown", event => { 
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   addTask();
+   event.target.value = "";
+  }
+});
+
+taskList.addEventListener("click", event => {
+ if (event.target.classList.contains("task__remove")) {
+  const taskId = event.target.closest(".task").getAttribute("data-task-id");
+  localStorage.removeItem(taskId);  
+  event.target.closest(".task").remove();
+ }
+});
